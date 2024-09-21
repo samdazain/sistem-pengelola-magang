59c199f9007ad9157e4e2929ba2c672e1de2cb62
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-09-21 09:00:34 +0700

    penambahan login dan register

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,10 +21,5 @@ class DatabaseSeeder extends Seeder
             LamaranSeeder::class,
             PenilaianSeeder::class,
         ]);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,27 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PelamarController;
 use App\Http\Controllers\PenilaianController;
 use App\Http\Controllers\PosisiController;
+use App\Http\Controllers\UserController; // Tambahkan UserController
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::resource('pelamar', PelamarController::class);
-Route::resource('posisi', PosisiController::class);
-Route::resource('penilaian', PenilaianController::class);
-Route::resource('admin', AdminController::class);
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+// Route untuk register
+Route::get('/register', [AuthController::class, 'create'])->name('register.create'); // Menampilkan form register
+Route::post('/register', [AuthController::class, 'store'])->name('register.store'); // Menyimpan data pengguna baru
+
+Route::middleware('auth')->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::resource('pelamar', PelamarController::class);
+    Route::resource('posisi', PosisiController::class);
+    Route::resource('penilaian', PenilaianController::class);
+    Route::resource('admin', AdminController::class);
+    Route::resource('users', UserController::class); // Menambahkan resource untuk User
+});
