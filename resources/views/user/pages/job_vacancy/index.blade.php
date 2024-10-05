ff5fa371ecb1914c19055154dd51d76353d3f370
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2024-10-05 16:25:52 +0700

    setup tailwind untuk halaman user

--- resources/views/user/pages/job_vacancy/index.blade.php
+++ resources/views/user/pages/job_vacancy/index.blade.php
@@ -4,13 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-  <h1>Halooo</h1>
+  <h1 class="text-red-500">Halooo</h1>
 
   <div class="d-grid mt-3">
     <form action="{{ route('logout') }}" method="POST">
         @csrf
+        <p class="text-blue-500">halo</p>
         <button type="submit" class="btn btn-danger">Logout</button>
     </form>
 </div>
